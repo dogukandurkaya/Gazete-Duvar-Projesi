94eec18e3727bb3a238802494c3818b7c16d170a
dogukandurkaya <98876145+dogukandurkaya@users.noreply.github.com>
2022-06-16 09:34:46 +0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,20 @@
+$(document).ready(function () {
+    $(".buyutec").click(function () {
+        $(".buyutec").hide(1000);
+        $(".ara").show(1000);
+    })
+})
+
+$(document).ready(function () {
+    $("#i-left").click(function () {
+        $("#first").hide("slow")
+        $("#second").show("slow")
+    })
+})
+
+$(document).ready(function () {
+    $("#pass-left").click(function () {
+        $("#1").hide(3000)
+        $(".writers").show(3000)
+    })
+})
